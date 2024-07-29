8b257b5bb68a541b0892c5e37307f21acf7926a9
KristoferPervin <7747148+kpervin@users.noreply.github.com>
2024-07-29 14:49:02 -0300

    6.3.0 dates

--- src/example.test.ts
+++ src/example.test.ts
@@ -1,15 +1,20 @@
 import { defineConfig, MikroORM } from "@mikro-orm/mysql";
 import { Post } from "./entities/post.entity";
 import { User } from "./entities/user.entity";
+import { TsMorphMetadataProvider } from "@mikro-orm/reflection";
 
 let orm: MikroORM;
 
 beforeAll(async () => {
   orm = await MikroORM.init(defineConfig({
     // dbName: ":memory:",
-    entities: [User, Post],
-    debug: ["query", "query-params"],
+    entities: [ User, Post ],
+    metadataProvider: TsMorphMetadataProvider,
+    debug: [ "query", "query-params" ],
     allowGlobalContext: true, // only for testing
+    metadataCache: {
+      pretty: true,
+    },
   }));
   await orm.schema.ensureDatabase();
   await orm.schema.refreshDatabase();
@@ -24,12 +29,8 @@ test("basic CRUD example", async () => {
   await orm.em.flush();
   orm.em.clear();
 
-  const user = await orm.em.findOneOrFail(User, { email: "foo" });
-  expect(user.name).toBe("Foo");
-  user.name = "Bar";
+  const user = await orm.em.fork().findOneOrFail(User, { email: "foo" });
+  expect(user.createdAt).toBeInstanceOf(Date);
   orm.em.remove(user);
   await orm.em.flush();
-
-  const count = await orm.em.count(User, { email: "foo" });
-  expect(count).toBe(0);
 });
